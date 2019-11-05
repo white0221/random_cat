07f156fbed032751c3f7b098cc8e09635ad733cb
white0221 <whiterm.r0221@gmail.com>
2019-11-05 18:23:11 +0900

    add gem 'flickraw' show random(?) cat image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  require 'flickraw'
+  require 'json'
+
+  def cat
+    FlickRaw.api_key = ENV['FLICKR_KEY']
+    FlickRaw.shared_secret = ENV['FLICKR_SECRET']
+    result = flickr.photos.search(:tags => 'cat')
+    random = Random.new
+    num = random.rand(0..result.length)
+    @image = "https://farm#{result[num].farm}.staticflickr.com/#{result[num].server}/#{result[num].id}_#{result[num].secret}.jpg"
+  end
 end
